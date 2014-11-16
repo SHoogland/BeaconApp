757f7207f35483eecd23915df3d02527012f3abb
Stephan Hoogland <stephan@shoogland.com>
2014-11-16 16:37:19 +0100

    Initial Commit

---
+++ BeaconApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconApp
+//
+//  Created by Stephan Hoogland on 16/11/14.
+//  Copyright (c) 2014 SHoogland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
